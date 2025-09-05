f6f68b63ea826679556d6677cc17ca669bf15ffd
wierbicki <daniel@wierbicki.org>
2025-09-05 18:04:21 +0200

    Alternative Lösung hinzugefügt mit einfachen Anführungsstrichen.

--- 4-funktionen-nutzen-eingebaute/uebungen/uebung-len-2.py
+++ 4-funktionen-nutzen-eingebaute/uebungen/uebung-len-2.py
@@ -1,7 +1,8 @@
 print("=== Textlänge ermitteln ===")
 
 text = input("Text: ")
-print(f"Der Text \"{text}\" hat {len(text)} Zeichen.")
+#print(f"Der Text \"{text}\" hat {len(text)} Zeichen.")
+print(f'Der Text "{text}" hat {len(text)} Zeichen.')
 
 print()
 input("Taste drücken...")
