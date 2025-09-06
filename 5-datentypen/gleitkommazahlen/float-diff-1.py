cc80b3485d241239bc9efed604745939004b90fa
wierbicki <daniel@wierbicki.org>
2025-09-06 13:06:06 +0200

    Dateien hochgeladen zur Demonstration von Rundungsfehlern bei Float.

---
+++ 5-datentypen/gleitkommazahlen/float-diff-1.py
@@ -0,0 +1,6 @@
+wert = 1.1 - 0.1
+#wert = 1.2 - 0.1
+print(wert)
+print(type(wert))
+
+input("Taste drücken...")
---
+++ 5-datentypen/gleitkommazahlen/float-diff-2.py
@@ -0,0 +1,9 @@
+#wert1 = 1.1 - 0.1
+wert1 = 1.2 - 0.1
+print(wert1)
+wert2 = 1.0
+print(wert2)
+
+print(wert1 - wert2)
+
+input("Taste drücken...")
