620ae02d0ef6ca716b1545d1065c795dbab4a701
wierbicki <daniel@wierbicki.org>
2025-09-07 13:36:27 +0200

    Codebeispiele zu Arithmetik und Datentypen hinzugefügt. Warum Typkonvertierung notwendig ist.

---
+++ 6-datentypen-praxis/arithmetik/input-subtraktion.py
@@ -0,0 +1,9 @@
+# wert1 = input("Eingabe: ")
+# wert2= input("Eingabe: ")
+
+wert1 = float(input("Eingabe: "))
+wert2 = float(input("Eingabe: "))
+
+print(wert1 - wert2)
+
+input("Taste drücken...")
