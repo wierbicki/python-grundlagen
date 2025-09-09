7204b388ffe84bacf3b74ddd19512d3a789e209c
wierbicki <daniel@wierbicki.org>
2025-09-09 21:42:00 +0200

    Übungen zu Typkonvertierung und try / except

--- 6-datentypen-praxis/arithmetik/input-addition.py
+++
@@ -1,9 +0,0 @@
-# wert1 = input("Eingabe: ")
-# wert2= input("Eingabe: ")
-
-wert1 = float(input("Eingabe: "))
-wert2 = float(input("Eingabe: "))
-
-print(wert1 + wert2)
-
-input("Taste drücken...")
