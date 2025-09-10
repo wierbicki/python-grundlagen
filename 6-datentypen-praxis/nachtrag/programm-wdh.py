3a07a9a509d29a7cb3d3f2c214b028f5045c0dd9
wierbicki <daniel@wierbicki.org>
2025-09-10 15:46:51 +0200

    Demonstration der while-Schleife (Programm wiederholen)

---
+++ 6-datentypen-praxis/nachtrag/programm-wdh.py
@@ -0,0 +1,12 @@
+while True:
+    print("=== Addition ===")
+
+    try:
+        zahl1 = float(input("Zahl 1: "))
+        zahl2 = float(input("Zahl 2: "))
+        print(f"{zahl1} + {zahl2} = {zahl1 + zahl2}")
+    except:
+        print("Fehler")
+
+    print("----------------")
+    input("Taste drücken...")
