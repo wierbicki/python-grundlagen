d0438969ab26f6691975f9bfca0bd87bb551fcfc
wierbicki <daniel@wierbicki.org>
2025-09-11 19:21:25 +0200

    Ordner diverses erstellt und dorthin Dateien verschoben.

---
+++ diverses/funktionen-attribute.py
@@ -0,0 +1,11 @@
+input(len.__name__)   # Name der Funktion
+input(len.__doc__)    # Dokumentation
+input(len.__class__)  # Typ der Funktion
+
+input(min.__name__)
+input(min.__doc__)
+input(min.__class__)
+
+input(max.__name__)
+input(max.__doc__)
+input(max.__class__)
