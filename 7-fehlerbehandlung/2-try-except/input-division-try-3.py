2619835795bcab0dd67b871ffb87c5bd06df2b60
wierbicki <daniel@wierbicki.org>
2025-09-12 22:18:55 +0200

    Quelltext angepasst, um den Unterschied zwischen Datentyp anzeigen und Name vom Datentyp anzeigen zu zeigen.

--- 7-fehlerbehandlung/2-try-except/input-division-try-3.py
+++ 7-fehlerbehandlung/2-try-except/input-division-try-3.py
@@ -3,6 +3,7 @@ try:
    zahl2 = float(input("Zahl 2: "))
    print(zahl1 / zahl2) 
 except Exception as e:
-   print(f"Fehlertyp: {type(e)}")
+   # print(f"Fehlertyp: {type(e)}")
+   print(f"Fehlertyp: {type(e).__name__}")
 
 input("Taste drücken...")
