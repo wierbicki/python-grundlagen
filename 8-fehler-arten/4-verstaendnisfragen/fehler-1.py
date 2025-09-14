e54beed83548876b132c7219de0d60f8960920e3
wierbicki <daniel@wierbicki.org>
2025-09-14 20:53:40 +0200

    Codebeispiele zur Demonstration von Fehlern generell.

---
+++ 8-fehler-arten/4-verstaendnisfragen/fehler-1.py
@@ -0,0 +1,6 @@
+print "Test" # Syntax
+print zahl   # Syntax
+print(z)     # Laufzeit (NameError)
+int("zwei")  # Laufzeit (ValueError)
+print("Test" # Syntax
+2zahl = 5    # Syntax
---
+++ 8-fehler-arten/4-verstaendnisfragen/fehler-2.py
@@ -0,0 +1,6 @@
+Print(5)       # Laufzeit (NameError)
+float("4,0")   # Laufzeit (ValueError)
+4 / 0          # Laufzeit (ZeroDivisionError)
+print("Test')  # Syntax
+zahl = "4" + 2 # Laufzeit (TypeError)
+
